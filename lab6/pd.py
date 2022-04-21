329943af635de2f2e46d64631dab18f5d177fd8a
kacperski36 <kacperski36@wp.pl>
2022-04-21 10:12:07 +0200

    done

--- lab6/pd.py
+++ lab6/pd.py
@@ -2,7 +2,8 @@ from unittest import result
 import numpy as np
 import math
 
-A = [[1, 1, 0], [0, 1, 1]]
+A = np.array([[1, 0], [1, 1], [0, 1]])
+# A = np.array(A)
 
 
 def kreseczki(vector):
@@ -13,33 +14,37 @@ def kreseczki(vector):
 
 
 def projekcja(u, v):
-    # u = np.array(u)
-    # v = np.array(v)
-    l = np.dot(v, u)
-    m = np.dot(u, u)
+    l = np.dot(v.T, u)
+    m = np.dot(u.T, u)
     return (l/m)*u
 
 
 def upo(A):
     Q = []
-    u1 = np.array(A[0])
+    v1 = []
+    v2 = []
+    for i in range(len(A)):
+        v1.append(A[i][0])
+        v2.append(A[i][1])
+    u1 = np.array(v1)
+    v2 = np.array(v2)
     e1 = u1/kreseczki(u1)
     Q.append(e1)
-    v2 = np.array(A[1])
     u2 = v2 - projekcja(u1, v2)
     e2 = u2/kreseczki(u2)
     Q.append(e2)
-    return np.array(Q)
+    Q = np.array(Q)
+    return np.array(Q.T)
 
 
 # print(kreseczki(A[0]))
 # print(projekcja(A[0], A[1]))
-
 Q = upo(A)
-R = np.dot(Q, A)
+R = np.dot(Q.T, A)
 xd = np.dot(Q, R)
 
 print(A)
 print(Q)
 print(R)
+print("nsdjnsjnfknskd")
 print(xd)
